98027892afd090847599f339c964a676cc29e698
OnescuAlex-Vlad <onescu8779@gmail.com>
2024-03-28 22:37:16 +0200

    Added error handling for the routes

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -20,45 +20,72 @@ export class OrderController {
 
   @Post()
   async createOrder(@Body() createOrderDto: CreateOrderDto) {
-    const order = await this.orderService.createOrder(createOrderDto);
-    return {
-      message: 'Order created successfully',
-      order: {
-        id: order._id,
-        status: order.status,
-        price: order.price,
-      },
-    };
+    try {
+      const order = await this.orderService.createOrder(createOrderDto);
+      return {
+        message: 'Order created successfully',
+        order: {
+          id: order._id,
+          status: order.status,
+          price: order.price,
+        },
+      };
+    } catch (error) {
+      return {
+        message: 'Failed to create order',
+        error: error.message,
+      };
+    }
   }
 
   @Post('/status')
   async updateOrderStatus(@Body() updateOrderStatusDto: UpdateOrderStatusDto) {
-    const order = await this.orderService.updateOrderStatus(
-      updateOrderStatusDto.id,
-      updateOrderStatusDto.status,
-    );
+    try {
+      const order = await this.orderService.updateOrderStatus(
+        updateOrderStatusDto.id,
+        updateOrderStatusDto.status,
+      );
 
-    return {
-      message: 'Order status updated successfully',
-      id: updateOrderStatusDto.id,
-      oldOrder: updateOrderStatusDto.status,
-      newStatus: order.status,
-    };
+      return {
+        message: 'Order status updated successfully',
+        id: updateOrderStatusDto.id,
+        oldOrder: updateOrderStatusDto.status,
+        newStatus: order.status,
+      };
+    } catch (error) {
+      return {
+        message: 'Failed to update order status',
+        error: error.message,
+      };
+    }
   }
 
   @Post('search')
+  /**
+   * Searches for orders by dropoff address.
+   *
+   * @param searchParams - The search parameters containing the address and postal code.
+   * @returns An object with a message and an array of order IDs.
+   */
   async searchOrderByDropoffAddress(
     @Body() searchParams: { address: string; postalCode: string },
   ) {
-    const { address, postalCode } = searchParams;
-    const orderIds = await this.orderService.searchOrderByDropoffAddress(
-      address,
-      postalCode,
-    );
-    return {
-      message: 'Orders found',
-      orderIds,
-    };
+    try {
+      const { address, postalCode } = searchParams;
+      const orderIds = await this.orderService.searchOrderByDropoffAddress(
+        address,
+        postalCode,
+      );
+      return {
+        message: 'Orders found',
+        orderIds,
+      };
+    } catch (error) {
+      return {
+        message: 'Failed to search orders by dropoff address',
+        error: error.message,
+      };
+    }
   }
 
   @Get('/list')
